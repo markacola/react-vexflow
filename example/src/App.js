79cfcb88f04a0edcfee906cd4f363f040b4c9a6a
Mark Cola <mark@doomsdaytuna.com>
2019-07-07 19:06:10 +1000

    added vexflow

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
-
-import ExampleComponent from 'react-vexflow'
+import { Score } from 'react-vexflow'
 
 export default class App extends Component {
-  render () {
+  render() {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <Score
+          staves={[
+            ['g3', 'd4', 'e4', 'd4'],
+            ['a4', 'd4', 'e4', 'd4'],
+            ['a4', 'a4', 'b4', 'a4'],
+            ['d4', 'e4', ['g3', 2]],
+          ]}
+        />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,22 +1,62 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { useRef, useEffect } from 'react'
+import VexFlow from 'vexflow'
 
-import styles from './styles.css'
+const VF = VexFlow.Flow
+const { Formatter, Renderer, Stave, StaveNote } = VF
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
+const clefAndTimeWidth = 60
 
-  render() {
-    const {
-      text
-    } = this.props
+export function Score({
+  staves = [],
+  clef = 'treble',
+  timeSignature = '4/4',
+  width = 450,
+  height = 150,
+}) {
+  const container = useRef()
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+  useEffect(() => {
+    const renderer = new Renderer(container.current, Renderer.Backends.SVG)
+    renderer.resize(width, height)
+    const context = renderer.getContext()
+    context.setFont('Arial', 10, '').setBackgroundFillStyle('#eed')
+    const staveWidth = (width - clefAndTimeWidth) / staves.length
+
+    let currX = 0
+    staves.forEach((notes, i) => {
+      const stave = new Stave(currX, 0, staveWidth)
+      if (i === 0) {
+        stave.setWidth(staveWidth + clefAndTimeWidth)
+        stave.addClef(clef).addTimeSignature(timeSignature)
+      }
+      currX += stave.getWidth()
+      stave.setContext(context).draw()
+
+      const processedNotes = notes
+        .map(note => (typeof note === 'string' ? { key: note } : note))
+        .map(note =>
+          Array.isArray(note) ? { key: note[0], duration: note[1] } : note
+        )
+        .map(({ key, ...rest }) =>
+          typeof key === 'string'
+            ? {
+                key: key.includes('/') ? key : `${key[0]}/${key.slice(1)}`,
+                ...rest,
+              }
+            : rest
+        )
+        .map(
+          ({ key, keys, duration = 'q' }) =>
+            new StaveNote({
+              keys: key ? [key] : keys,
+              duration: String(duration),
+            })
+        )
+      Formatter.FormatAndDraw(context, stave, processedNotes, {
+        auto_beam: true,
+      })
+    })
+  }, [staves])
+
+  return <div ref={container} />
 }
